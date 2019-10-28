d9a7864bc64e4f706807f36224ae03d088898303
April Ding <aprilding@Aprils-iMac.local>
2019-10-28 18:07:13 -0400

    week6day1Project

--- js/index.js
+++ js/index.js
@@ -39,4 +39,51 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let navItem = document.querySelectorAll("nav a");
+navItem[0].textContent = siteContent["nav"]["nav-item-1"];
+navItem[1].textContent = siteContent["nav"]["nav-item-2"];
+navItem[2].textContent = siteContent["nav"]["nav-item-3"];
+navItem[3].textContent = siteContent["nav"]["nav-item-4"];
+navItem[4].textContent = siteContent["nav"]["nav-item-5"];
+navItem[5].textContent = siteContent["nav"]["nav-item-6"];
+
+const newNavItem1 = document.createElement("a");
+newNavItem1.textContent = "New1";
+
+const newNavItem2 = document.createElement("a");
+newNavItem2.textContent = "New2";
+
+let navBar = document.querySelector("nav");
+navBar.prepend(newNavItem1);
+navBar.append(newNavItem2);
+
+navItem.forEach(item =>{ item.style.color = "green";});
+
+document.querySelector("h1").textContent = siteContent["cta"]["h1"];
+
+document.querySelector(".cta-text button").textContent = siteContent["cta"]["button"];
+
+document.querySelector("#cta-img").setAttribute("src", siteContent["cta"]["img-src"]);
+
+document.querySelector("#middle-img").setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+let headline4 = document.querySelectorAll("h4");
+headline4[0].textContent = siteContent["main-content"]["features-h4"];
+headline4[1].textContent = siteContent["main-content"]["about-h4"];
+headline4[2].textContent = siteContent["main-content"]["services-h4"];
+headline4[3].textContent = siteContent["main-content"]["product-h4"];
+headline4[4].textContent = siteContent["main-content"]["vision-h4"];
+headline4[5].textContent = siteContent["contact"]["contact-h4"];
+
+let allParagraph = document.querySelectorAll("p");
+allParagraph[0].textContent = siteContent["main-content"]["features-content"];
+allParagraph[1].textContent = siteContent["main-content"]["about-content"];
+allParagraph[2].textContent = siteContent["main-content"]["services-content"];
+allParagraph[3].textContent = siteContent["main-content"]["product-content"];
+allParagraph[4].textContent = siteContent["main-content"]["vision-content"];
+allParagraph[5].textContent = siteContent["contact"]["address"];
+allParagraph[6].textContent = siteContent["contact"]["phone"];
+allParagraph[7].textContent = siteContent["contact"]["email"];
+allParagraph[8].textContent = siteContent["footer"]["copyright"];
